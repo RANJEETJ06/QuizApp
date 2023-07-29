c360507ed87425773b204d5c7ce9b208462c889c
Ranjeet Kumar Jena <ranjeetjena06@gmai.com>
2023-07-29 11:49:12 +0530

    login constructor modified

--- MainFiles/Login.java
+++ MainFiles/Login.java
@@ -9,55 +9,9 @@ public class Login extends JFrame implements ActionListener {
     JButton play,close;
     JTextArea inputName;
     public static void main(String[] args) {
-        new Login();
+        new Login("");
     }
-    public Login(){
-        setTitle("Quiz Time");
-        ImageIcon wl=new ImageIcon(ClassLoader.getSystemResource("icons/ic.png"));
-        Image wl2=wl.getImage().getScaledInstance(500,500,Image.SCALE_DEFAULT);
-        setIconImage(wl2);
-
-        getContentPane().setBackground(Color.cyan);
-        setLayout(null);
-        ImageIcon i=new ImageIcon(ClassLoader.getSystemResource("icons/login.jpeg"));
-        JLabel image=new JLabel(i);
-        image.setBounds(20,80,400,300);
-        add(image);
-        setSize(900,500);
-        setLocation(200,100);
-        setVisible(true);
-
-        JLabel heading=new JLabel("Simple Quiz");
-        heading.setBounds(515,60,500,50);
-        heading.setFont(new Font("Viner Hand ITC",Font.BOLD,40));
-        heading.setForeground(Color.blue);
-        add(heading);
-
-        JLabel name=new JLabel("Enter Your Name");
-        name.setBounds(570,110,300,50);
-        name.setFont(new Font("Times New Roman", Font.BOLD,20));
-        name.setForeground(Color.blue);
-        add(name);
-
-        inputName=new JTextArea();
-        inputName.setText("");
-        inputName.setBounds(500,170,300,27);
-        inputName.setFont(new Font("Times New Roman",Font.BOLD,20));
-        add(inputName);
-
-        play=new JButton("Play");
-        play.setBounds(680,250,80,27);
-        play.setFont(new Font("Times New Roman", Font.PLAIN,15));
-        play.addActionListener(this);
-        add(play);
-
-        close=new JButton("Exit");
-        close.setBounds(525,250,80,27);
-        close.setFont(new Font("Times New Roman", Font.PLAIN,15));
-        close.addActionListener(this);
-        add(close);
-    }
-    public Login(String back_name){
+    public Login(String LoginName){
         setTitle("Quiz Time");
         ImageIcon wl=new ImageIcon(ClassLoader.getSystemResource("icons/ic.png"));
         Image wl2=wl.getImage().getScaledInstance(500,500,Image.SCALE_DEFAULT);
@@ -85,7 +39,7 @@ public class Login extends JFrame implements ActionListener {
         add(name);
 
         inputName=new JTextArea();
-        inputName.setText(back_name);
+        inputName.setText(LoginName);
         inputName.setBounds(500,170,300,27);
         inputName.setFont(new Font("Times New Roman",Font.BOLD,20));
         add(inputName);
